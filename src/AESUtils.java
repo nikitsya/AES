716e6d8972730e99f7b6059ffef81f50d71b3172
nikitsya <nikitasmiichyk@gmail.com>
2025-12-03 19:58:06 +0000

    add a generateRandomKey function

--- src/AESUtils.java
+++ src/AESUtils.java
@@ -1,4 +1,6 @@
 import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Arrays;
 
 public class AESUtils {
 
@@ -7,6 +9,10 @@ public class AESUtils {
         String text = "test";
         String hexText = toHex(text);
         System.out.println(text + ": " + hexText);
+
+        byte[] key128 = generateRandomKey(128);
+        System.out.println("128-bit key: " + toHex(Arrays.toString(key128)));
+
     }
 
     public static String encrypt(String text) {
@@ -35,5 +41,19 @@ public class AESUtils {
             sb.append(String.format("%02X", b));
         }
         return sb.toString();
+    }
+
+    public static byte[] generateRandomKey(int bits) {
+        if (bits % 8 != 0) {
+            throw new IllegalArgumentException("Key size must be a multiple of 8 bits");
+        }
+
+        int bytes = bits / 8;
+        SecureRandom random = new SecureRandom();
+
+        byte[] key = new byte[bytes];
+        random.nextBytes(key);
+
+        return key;
     }
 }
