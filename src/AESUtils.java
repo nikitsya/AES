9a4dea06252f764e44f68a3d1d8d6f877d450065
nikitsya <nikitasmiichyk@gmail.com>
2025-12-03 12:08:03 +0000

    add encrypt and decrypt function body

--- src/AESUtils.java
+++ src/AESUtils.java
@@ -1,3 +1,24 @@
 public class AESUtils {
 
+    // This method use to encrypt
+    public static String encrypt(String string) {
+        try {
+
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e);
+        }
+
+        return null;
+    }
+
+    // This method use to decrypt
+    public static String decrypt(String string) {
+        try {
+
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e);
+        }
+
+        return null;
+    }
 }
