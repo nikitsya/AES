c17f1d3edfb31bb3d3874e058611fdc6407055be
nikitsya <nikitasmiichyk@gmail.com>
2025-12-04 20:51:03 +0000

    add Menu.printMainMenu

--- src/Menu.java
+++ src/Menu.java
@@ -4,8 +4,29 @@ import java.util.Scanner;
 
 public class Menu {
 
+    private static final String invalid_input_message = "Invalid input. Please try again.";
+
     public static void menu() {
         Scanner user_input = new Scanner(System.in);
+        printMainMenu();
+        try {
+            System.out.print("Write a number (1-3): ");
+            int choice = user_input.nextInt();
+
+            if (choice == 1) {
+                System.out.print(" - - - ");
+            } else if (choice == 2) {
+                System.out.print(" - - - ");
+            } else {
+                System.out.println(invalid_input_message);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(invalid_input_message);
+            menu();
+        }
+    }
+
+    private static void printMainMenu() {
         System.out.println("""
             
             
@@ -17,18 +38,5 @@ public class Menu {
             2. Decrypt a File
             3. Quit the application
             """);
-        try {
-            System.out.print("Write a number (1-3): ");
-            int choice = user_input.nextInt();
-
-            if (choice == 1) {
-                System.out.print(" - - - ");
-            } else if (choice == 2) {
-                System.out.print(" - - - ");
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please try again.");
-            menu();
-        }
     }
 }
