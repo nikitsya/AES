e71787b263eee089826fdb5a0e0779d4fe09484c
nikitsya <nikitasmiichyk@gmail.com>
2025-12-04 15:04:39 +0000

    add FileUtils.getLines() that return list of lines from a file

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -1,3 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class FileUtils {
 
+    public static void main(String[] args) {
+        String plain_text_path = "plaintext.txt";
+        String cipher_text_path = "ciphertext.txt";
+        byte[] key = AESUtils.generate16ByteRandomKey();
+
+        System.out.println(getLines(plain_text_path));
+    }
+
+    public static List<String> getLines(String path) {
+        try {
+            return Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            System.out.println("Error while reading file: " + e);
+        }
+        return null;
+    }
 }
