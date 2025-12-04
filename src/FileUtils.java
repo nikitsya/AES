e850e71f24c52fef7852c206d6c1502af328037e
nikitsya <nikitasmiichyk@gmail.com>
2025-12-04 20:32:57 +0000

    add FileUtils.writeFile() function

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
@@ -27,5 +26,13 @@ public class FileUtils {
             System.out.println("Error while reading file: " + e);
         }
         return null;
+    }
+
+    public static void writeFile(String path, String content) {
+        try {
+            Files.write(Paths.get(path), content.getBytes());
+        } catch (IOException e) {
+            System.out.println("Error while writing file: " + e);
+        }
     }
 }
