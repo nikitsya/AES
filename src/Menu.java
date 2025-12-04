a26b8481567d0d54987531f720733152ca2b489a
nikitsya <nikitasmiichyk@gmail.com>
2025-12-04 20:59:23 +0000

    Refactor menu logic

--- src/Menu.java
+++ src/Menu.java
@@ -4,25 +4,32 @@ import java.util.Scanner;
 
 public class Menu {
 
+    private static Scanner user_input = new Scanner(System.in);
     private static final String invalid_input_message = "Invalid input. Please try again.";
+    private static final String write_number_message = "Write a number (1-3): ";
 
     public static void menu() {
-        Scanner user_input = new Scanner(System.in);
-        printMainMenu();
-        try {
-            System.out.print("Write a number (1-3): ");
-            int choice = user_input.nextInt();
+        while (true) {
+            printMainMenu();
+            try {
+                System.out.print(write_number_message);
+                int choice = user_input.nextInt();
 
-            if (choice == 1) {
-                System.out.print(" - - - ");
-            } else if (choice == 2) {
-                System.out.print(" - - - ");
-            } else {
+                if (choice == 1) {
+                    // TODO
+                } else if (choice == 2) {
+                    // TODO
+                } else if (choice == 3) {
+                    System.out.println("Exiting...");
+                    break;
+                } else {
+                    System.out.println(invalid_input_message);
+                }
+
+            } catch (InputMismatchException e) {
                 System.out.println(invalid_input_message);
+                user_input.nextLine();
             }
-        } catch (InputMismatchException e) {
-            System.out.println(invalid_input_message);
-            menu();
         }
     }
 
