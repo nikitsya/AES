c9c62c84ae04065364954d5ef64d1753f399552b
nikitsya <nikitasmiichyk@gmail.com>
2025-12-05 22:21:29 +0000

    add docstring to the App class

--- src/App.java
+++ src/App.java
@@ -1,3 +1,8 @@
+/**
+ * Entry point of the application.
+ * This class simply launches the program by calling the main menu
+ * and handling all user interactions through the Menu class.
+ */
 public class App {
 
     public static void main(String[] args) {
